27bd5a6814ba47aab563168b5896ab9bd9637100
Expo <crdailey01@gmail.com>
2023-09-22 20:58:23 -0700

    My solution, beats 92% in efficiency, 80% in memory. Should be O(n) since just one loop.

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,49 @@ public class Main {
     }
 }
 
+// [3,-2,-2,3,-2, -5, 10]
+// MY SOLUTION, SUCK MY LEFT NUT, LEETCODE
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int sum = 0;
-        for(int i = 0; i<gas.length; i++){
-            sum+= (gas[i] - cost[i]);
-        }
+        int curSum = 0;
+        int preSum = 0;
+        int L = 0;
+        int curr = 0;
 
+        while (curr < gas.length) {
+            curSum += gas[curr] - cost[curr];
+            if (curSum < 0) {
+                preSum += curSum;
+                curSum = 0;
+                L = curr + 1;
+            }
+            curr++;
+        }
+        if ((preSum + curSum) < 0) {
+            return -1;
+        } else {
+            return L;
+
+        }
     }
 }
+
+// maybe need currSum vs priorSum?
+// take 3,-2,-2,3,-2
+// index 0 to 2 = -1 sum. store to priorSum and say starting index 3 to 4 cur sum is 1
+// prior sum + cur sum >= 0
+// suppose a section is - then + then - then +
+// [3,-2,-2,3,-2, -5, 10]. total sum is 5. sections of -1, -4, 6. every time seection is neg add to prriorSUm
+
+//        for(int i = 0; i<gas.length; i++){
+//            sum+= (gas[i] - cost[i]);
+//        }
+
 
 // not boolean return. need int.
 // will need L pointer such that if gas[i]- cost[i] >=0 then proceed with loop and store the index at pointer
 // if sum ever dips <0 it means the preceding order did not result in sufficient gas and that starting sequence can
 // be discarded. Seems similar to Maximum subarray leetcode
-
 
 
 // unitsOfGas
